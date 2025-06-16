695480e909c93bab46c89af36ef11cb96abc1507
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2025-06-16 15:13:43 -0700

    Create One Grid Square
    Created one grid square inside container.

---
+++ script.js
@@ -0,0 +1,7 @@
+const containter = document.querySelector(".container");
+
+const box = document.createElement("div");
+box.style.height = "16px";
+box.style.width = "16px";
+containter.appendChild(box);
+
