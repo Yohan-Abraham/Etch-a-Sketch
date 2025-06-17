817b88882212465925eadf6a76ff8b5cc812569d
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2025-06-16 17:04:46 -0700

    Add Resizing Feature
    Added resizing feature to grid.

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const button = document.querySelector(".btn");
+const container = document.querySelector(".container");
+const button = document.querySelector("#btn");
+const CONTAINER_SIZE = 960;
 
+function generateGrid(size) {
+    container.innerHTML = "";
 
+    const boxSize = CONTAINER_SIZE / size;
 
-
-
-const containter = document.querySelector(".container");
-
-for (let i = 0; i < 256; i++) {
-
-    const box = document.createElement("div");
-    box.classList.add("box");
-    containter.appendChild(box);
+    for (let i = 0; i < (size * size); i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.width = `${boxSize}px`;
+        box.style.height = `${boxSize}px`;
+        box.addEventListener("mouseover", () => {
+            box.style.background = "red";
+        });
+        container.appendChild(box);
+    }
 }
 
-const square = document.querySelectorAll(".box");
+button.addEventListener("click", () => {
+    let newSize = prompt("Enter a new size for the grid. Max size is 100");
+    newSize = parseInt(newSize);
 
-square.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "blue";
-    });
+    if (newSize > 100) {
+        alert("The number is greater than 100!");
+        return;
+    }
+
+    generateGrid(newSize);
 });
+
+generateGrid(16);
